b154db7ec1456808ea46b83bb35a2b0fbe4dac9c
glfruit <glfruit@glfruitx.local>
2014-06-17 15:43:04 +0800

    Initial Commit

---
+++ iOS7Tutorial/HYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYAppDelegate.h
+//  iOS7Tutorial
+//
+//  Created by Fruit Lee on 14-6-17.
+//  Copyright (c) 2014年 Fruit Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS7Tutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS7Tutorial
+//
+//  Created by Fruit Lee on 14-6-17.
+//  Copyright (c) 2014年 Fruit Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYAppDelegate class]));
+    }
+}
